6018e54718f5c5faaa9c88993b7f11ac50dd9e3d
Baumdiggity <atomnomicbomb@gmail.com>
2020-07-24 16:34:33 -0700

    Can now point and click "pineapples"

---
+++ Assets/Scripts/PineappleSelector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PineappleSelector : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.gameObject.CompareTag("Pineapple"))
+                {
+                    Debug.Log("That's a Pineapple");
+                    hit.transform.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+}
