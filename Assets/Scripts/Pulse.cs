6df0b43df512100403793ff0ae029fe39b258dca
Baumdiggity <atomnomicbomb@gmail.com>
2020-07-25 15:32:34 -0700

    Created some basic animations, implemented BackBeatPulse Bloom effect

---
+++ Assets/Scripts/Pulse.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class Pulse : MonoBehaviour
+{
+    public Volume postProcessVolume;
+
+    Bloom _bloom;
+
+    public float bloomIntensity = 1f;
+    //public float pulseSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        postProcessVolume.profile.TryGet(out _bloom);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //_bloom.intensity.value = Mathf.PingPong(Time.time*pulseSpeed, bloomIntensity)*500f;
+        _bloom.intensity.value = bloomIntensity;
+    }
+}
