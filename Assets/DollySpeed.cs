20d1b9be66828bf2a6e0b9143e9ee09068ffecfb
Baumdiggity <atomnomicbomb@gmail.com>
2020-07-26 00:20:31 -0700

    Finished intro animation

---
+++ Assets/DollySpeed.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class DollySpeed : MonoBehaviour
+{
+
+    CinemachineDollyCart cart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cart = GetComponent<CinemachineDollyCart>();
+    }
+
+    public void ChangeSpeed(float spd)
+    {
+        cart.m_Speed = spd;
+    }
+}
---
+++ Assets/Scripts/ParentManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParentManager : MonoBehaviour
+{
+    public GameObject car;
+    public GameObject dolly;
+    public GameObject cam;
+
+
+    public Vector3 carPos, dollyPos, cameraPos; //-1.8f, 1.75f, -1.25f   0,0,5f
+
+
+    public void SwapParent(int par)
+    {
+        switch (par)
+        {
+            case 0: gameObject.transform.parent = car.transform;
+                gameObject.transform.localPosition = carPos;
+                break;
+            case 1: gameObject.transform.parent = dolly.transform;
+                gameObject.transform.localPosition = dollyPos;
+                break;
+            case 2: gameObject.transform.parent = cam.transform;
+                gameObject.transform.localPosition = cameraPos;
+                break;
+        }
+    }
+}
